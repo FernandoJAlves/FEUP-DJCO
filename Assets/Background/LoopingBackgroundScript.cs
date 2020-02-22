f31d7beb8929420cd13e2df7a85593130daf6996
Raidenkyu <joao-carlos.14@hotmail.com>
2020-02-22 16:36:04 +0000

    Added Scrolling background

---
+++ Assets/Background/LoopingBackgroundScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingBackgroundScript : MonoBehaviour
+{
+    private Renderer background;
+
+    [SerializeField]
+    private float scrollSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Renderer> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2 (scrollSpeed * Time.deltaTime, 0);
+ 
+        background.material.mainTextureOffset += offset;
+    }
+}
---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move Character in the X and Y Axis
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        Vector3 vec = new Vector3(deltaX,deltaY, 0);
+
+        // Move the Character:
+        transform.Translate(vec);
+    }
+}
