1638b142a725f5411d0a956e52feb78906dafdf0
Fernando Alves <up201605270@fe.up.pt>
2020-02-29 23:17:44 +0000

    First iteration of enemy spawning logic

---
+++ Assets/EnemySpawningScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class EnemySpawningScript : MonoBehaviour
+{
+    // List of enemies
+    public GameObject electroBot;
+
+    public float spawnRate = 4f;
+    float nextSpawn = 0f;
+
+    // Enum of valid enemies
+    public enum NextEnemyType {
+        ELECTROBOT
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (Time.time > nextSpawn) {
+            spawnEnemy();
+        }
+
+    }
+
+    private void spawnEnemy() {
+
+        nextSpawn = Time.time + spawnRate;        
+        NextEnemyType nextEnemyType = getNextEnemy();
+
+        switch (nextEnemyType)
+        {
+            case NextEnemyType.ELECTROBOT:
+                float randOffsetY = Random.Range(-0.5f, 0.5f);
+                Vector3 spawnPoint = new Vector3 (3f, 1.4f + randOffsetY, -5f);
+                Instantiate (electroBot, spawnPoint, Quaternion.identity);
+                break;
+
+            default:
+                break;
+        }
+
+
+    }
+
+    private NextEnemyType getNextEnemy() {
+        float randValue = Random.Range(0f, 1f);
+        
+        if (randValue >= 0f) {
+            return NextEnemyType.ELECTROBOT;
+        }
+
+        Debug.LogError("Enexpected return statement reached");
+        return NextEnemyType.ELECTROBOT;
+    }
+}
