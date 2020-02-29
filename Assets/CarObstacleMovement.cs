17e7d811eb75fd210ad5a857b1b0611395c4baee
Fernando Alves <up201605270@fe.up.pt>
2020-02-29 23:43:12 +0000

    First iteration of car obstacle movement

---
+++ Assets/CarObstacleMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CarObstacleMovement : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void FixedUpdate() {
+        transform.position += new Vector3(-1, 0, 0) * movementSpeed * Time.deltaTime;
+    }
+}
