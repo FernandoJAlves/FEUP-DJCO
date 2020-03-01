cd676be139ba0052eef63561915683d791cfa806
Raidenkyu <joao-carlos.14@hotmail.com>
2020-03-01 21:08:42 +0000

    Added bolt shooting and camera limits

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 4.0f;
+    public Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo) {
+        Debug.Log(hitInfo.name);
+    }
+}
---
+++ Assets/Scripts/ScreenBoundaries.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBoundaries : MonoBehaviour
+{
+
+    //The Screen Boundaries
+    private Vector2 ScreenBounds;
+    private float playerWidth;
+    private float playerHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        playerHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, -1 * ScreenBounds.x, ScreenBounds.x);
+        viewPos.y = Mathf.Clamp(viewPos.y, -1 * ScreenBounds.y + 2 * playerHeight, ScreenBounds.y);
+        transform.position = viewPos;
+    }
+}
