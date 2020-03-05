08f1536bca1c11e57168fef3a12bab91fd1ac092
Raidenkyu <joao-carlos.14@hotmail.com>
2020-03-05 16:08:31 +0000

    Added Player Health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    // The current health the player has.
+    public int healthPoints;
+
+    //The reference to the player
+    
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TakeDamage() {
+        this.healthPoints--;
+
+        if (this.healthPoints <= 0) {
+
+        }
+    }
+}
