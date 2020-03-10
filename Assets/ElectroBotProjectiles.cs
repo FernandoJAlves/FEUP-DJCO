47a119818f07e4e6e3408bc2606b36e67175340e
Fernando Alves <up201605270@fe.up.pt>
2020-03-10 18:14:57 +0000

    Added logic for ElectroBots to fire a bolt every few seconds

---
+++ Assets/ElectroBotProjectiles.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ElectroBotProjectiles : MonoBehaviour
+{
+
+    public float timeSinceLastShot = 0f;
+    public float maxTimeBetweenShots = 2f; // fire once every 2 seconds
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastShot += Time.deltaTime;
+        if (timeSinceLastShot >= maxTimeBetweenShots) {
+            // now the bot should fire
+            timeSinceLastShot = 0f;
+            // FIRE!
+            Debug.Log("FIRE!!!");
+        }
+    }
+}
