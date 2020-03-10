8e7303d17e9bc8dd5a565d4b1132114dd20f0854
Fernando Alves <up201605270@fe.up.pt>
2020-03-10 18:40:52 +0000

    ElectroBots now fire bolts, however they still kill themselves

---
+++ Assets/Scripts/ElectroBotBolt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ElectroBotBolt : MonoBehaviour
+{
+    public float speed = 4.0f;
+    public Rigidbody2D body;
+    
+    // Start is called before the first frame update
+    void Start() {
+        body.velocity = -transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log("ELETRO BOLT HIT SOMETHING!!!");
+        Destroy(gameObject);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
+
--- Assets/Scripts/ElectroBotProjectiles.cs
+++ Assets/Scripts/ElectroBotProjectiles.cs
@@ -6,6 +6,8 @@ public class ElectroBotProjectiles : MonoBehaviour
     public float timeSinceLastShot = 0f;
     public float maxTimeBetweenShots = 2f; // fire once every 2 seconds
 
+    public GameObject eletroBotBoltPrefab;
+
     // Update is called once per frame
     void Update()
     {
@@ -14,7 +16,12 @@ public class ElectroBotProjectiles : MonoBehaviour
             // now the bot should fire
             timeSinceLastShot = 0f;
             // FIRE!
-            Debug.Log("FIRE!!!");
+            Shoot();
         }
+    }
+
+    public void Shoot() {
+        Debug.Log("FIRE!!!");
+        Instantiate(eletroBotBoltPrefab, transform.position, Quaternion.identity);
     }
 }
