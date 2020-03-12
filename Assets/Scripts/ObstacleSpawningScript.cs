e26884ce1d58b0ff7bb0078df3e2562af8d4b0c8
Fernando Alves <up201605270@fe.up.pt>
2020-03-12 16:07:40 +0000

    Added dynamic spawning of cars

---
+++ Assets/Scripts/ObstacleSpawningScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ObstacleSpawningScript : MonoBehaviour
+{
+    // List of obstacles
+    public GameObject carObstacle;
+    
+    public float spawnRate = 8f;
+    float nextSpawn = 0f;
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (Time.time > nextSpawn) {
+            spawnObstacle();
+        }
+
+    }
+
+    private void spawnObstacle() {
+
+        nextSpawn = Time.time + spawnRate;        
+
+        // spawn a car
+        float randOffsetY = Random.Range(-1.2f, 1.2f);
+        Vector3 spawnPoint = new Vector3 (7f, randOffsetY, -5f);
+        Instantiate (carObstacle, spawnPoint, Quaternion.identity);
+
+    }
+}
