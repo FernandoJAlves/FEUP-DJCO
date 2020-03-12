131dfcd57b6f281b6dfc56f09c54f206e6a6563c
Raidenkyu <joao-carlos.14@hotmail.com>
2020-03-12 19:01:53 +0000

    Added Timer to Game Scene

---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    private int seconds = 0;
+    private string timerLabel = "TIMER: ";
+    public Text timerText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds = (int) GameStateController.instance.playTime;
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+        timerText.text = this.timerLabel + minutes.ToString("00") + ':' + seconds.ToString("00");
+    }
+}
