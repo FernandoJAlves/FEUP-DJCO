31ed029f287b20457dd4dfb63217425e72979b2e
Fernando Alves <up201605270@fe.up.pt>
2020-03-13 15:42:40 +0000

    Added MecGirl to enemy spawner and started a basic type of movement for her

--- Assets/Scripts/EnemySpawningScript.cs
+++ Assets/Scripts/EnemySpawningScript.cs
@@ -4,13 +4,14 @@ public class EnemySpawningScript : MonoBehaviour
 {
     // List of enemies
     public GameObject electroBot;
+    public GameObject mecGirl;
 
-    public float spawnRate = 4f;
+    public float spawnRate = 2.5f;
     float nextSpawn = 0f;
 
     // Enum of valid enemies
     public enum NextEnemyType {
-        ELECTROBOT
+        ELECTROBOT, MECGIRL
     }
 
     // Update is called once per frame
@@ -29,11 +30,19 @@ public class EnemySpawningScript : MonoBehaviour
 
         switch (nextEnemyType)
         {
-            case NextEnemyType.ELECTROBOT:
+            case NextEnemyType.ELECTROBOT: {
                 float randOffsetY = Random.Range(-0.5f, 0.5f);
                 Vector3 spawnPoint = new Vector3 (6f, 1.4f + randOffsetY, -5f);
                 Instantiate (electroBot, spawnPoint, Quaternion.identity);
                 break;
+            }
+
+            case NextEnemyType.MECGIRL: {
+                float randOffsetY = Random.Range(-0.3f, 0.3f);
+                Vector3 spawnPoint = new Vector3 (6f, -1.3f + randOffsetY, -5f);
+                Instantiate (mecGirl, spawnPoint, Quaternion.identity);
+                break;
+            }
 
             default:
                 break;
@@ -45,8 +54,13 @@ public class EnemySpawningScript : MonoBehaviour
     private NextEnemyType getNextEnemy() {
         float randValue = Random.Range(0f, 1f);
         
-        if (randValue >= 0f) {
+        // 50% of ElectroBot
+        if (randValue <= 0.5f) {
             return NextEnemyType.ELECTROBOT;
+        }
+        // 50% of MecGirl
+        else if (randValue <= 1f) {
+            return NextEnemyType.MECGIRL;
         }
 
         Debug.LogError("Enexpected return statement reached");
---
+++ Assets/Scripts/MecGirlMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MecGirlMovement : MonoBehaviour
+{
+
+
+    // movement variables
+    public float movementSpeed = 0.7f;
+
+    // Bot destruction score
+    public int destructionScore = 30;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    private void FixedUpdate() {
+        transform.position += new Vector3(-1, 0, 0) * movementSpeed * Time.deltaTime;
+    }
+
+    void OnBecameInvisible()
+    {
+        // only destroy if on the left of the screen
+        if (transform.position.x <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log("MecGirl collided with " + hitInfo.name);
+        GameStateController.instance.Score(this.destructionScore);
+        Destroy(gameObject);
+    }
+
+}
