15fb1ee297cde42bd3ac716795c53ca810597738
Fernando Alves <up201605270@fe.up.pt>
2020-03-13 15:59:31 +0000

    Added AOC power up to the power up spawner

--- Assets/Scripts/PowerUpSpawningScript.cs
+++ Assets/Scripts/PowerUpSpawningScript.cs
@@ -5,13 +5,14 @@ public class PowerUpSpawningScript : MonoBehaviour
 
     // List of power ups
     public GameObject calPowerUp;
+    public GameObject aocPowerUp;
 
     public float spawnRate = 6f;
     float nextSpawn = 0f;
 
     // Enum of valid power ups
     public enum NextPowerType {
-        CAL_POWER
+        CAL_POWER, AOC_POWER
     }
 
     // Update is called once per frame
@@ -28,11 +29,19 @@ public class PowerUpSpawningScript : MonoBehaviour
 
         switch (nextEnemyType)
         {
-            case NextPowerType.CAL_POWER:
+            case NextPowerType.CAL_POWER: {
                 float randOffsetY = Random.Range(-1.2f, 1.2f);
                 Vector3 spawnPoint = new Vector3 (7f, randOffsetY, -5f);
                 Instantiate (calPowerUp, spawnPoint, Quaternion.identity);
                 break;
+            }
+
+            case NextPowerType.AOC_POWER: {
+                float randOffsetY = Random.Range(-1.2f, 1.2f);
+                Vector3 spawnPoint = new Vector3 (7f, randOffsetY, -5f);
+                Instantiate (aocPowerUp, spawnPoint, Quaternion.identity);
+                break;
+            }
 
             default:
                 break;
@@ -44,9 +53,13 @@ public class PowerUpSpawningScript : MonoBehaviour
         
         float randValue = Random.Range(0f, 1f);
         
-        // currently always spawns CAL_POWER
-        if (randValue >= 0f) {
+        // 50% of CAL_POWER
+        if (randValue <= 0.5f) {
             return NextPowerType.CAL_POWER;
+        }
+        // 50% of AOC_POWER
+        else if (randValue <= 1f) {
+            return NextPowerType.AOC_POWER;
         }
 
         Debug.LogError("Enexpected return statement reached");
