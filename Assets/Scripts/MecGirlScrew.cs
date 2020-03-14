6136e457ba9c018e3752dc564a739eb1be036ddd
Fernando Alves <up201605270@fe.up.pt>
2020-03-14 21:32:48 +0000

    Added screw rotate animation

--- Assets/Scripts/MecGirlScrew.cs
+++ Assets/Scripts/MecGirlScrew.cs
@@ -3,8 +3,19 @@
 public class MecGirlScrew : MonoBehaviour
 {
     public Vector2 force = new Vector2(-180.0f, 300.0f);
-    
+    public float timeBetweenRotates = 0.2f;
+
+    private float nextRotate = 0f;
     private bool firstTime = true;
+
+    private void Update() {
+
+        if (Time.time > nextRotate) {
+            nextRotate = Time.time + timeBetweenRotates;
+            transform.Rotate(Vector3.forward * 90);
+        }
+
+    }
 
     void FixedUpdate () {
         if (firstTime) {
