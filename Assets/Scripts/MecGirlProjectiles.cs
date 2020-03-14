b5a17ee0c02e122adc6876c23aefc3525e717723
Fernando Alves <up201605270@fe.up.pt>
2020-03-14 18:31:57 +0000

    Added screw launching script

--- Assets/Scripts/MecGirlProjectiles.cs
+++ Assets/Scripts/MecGirlProjectiles.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MecGirlProjectiles : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float timeSinceLastShot = 0f;
+    public float maxTimeBetweenShots = 2f; // fire once every 2 seconds
+
+    public GameObject mecGirlScrewPrefab;
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeSinceLastShot += Time.deltaTime;
+        if (timeSinceLastShot >= maxTimeBetweenShots) {
+            // now the bot should fire
+            timeSinceLastShot = 0f;
+            // FIRE!
+            Shoot();
+        }
+    }
+
+    public void Shoot() {
+        Debug.Log("FIRE!!!");
+        Instantiate(mecGirlScrewPrefab, transform.position, Quaternion.identity);
     }
 }
---
+++ Assets/Scripts/MecGirlScrew.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MecGirlScrew : MonoBehaviour
+{
+    public Vector2 force = new Vector2(-180.0f, 300.0f);
+    
+    private bool firstTime = true;
+
+    void FixedUpdate () {
+        if (firstTime) {
+            GetComponent<Rigidbody2D>().AddForce(force);
+            firstTime = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo) {
+        Debug.Log("MEC GIRL SCREW HIT SOMETHING!!!");
+        Destroy(gameObject);
+    }
+
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
