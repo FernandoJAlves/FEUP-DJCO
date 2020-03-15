5bed02b18f3241caaedff96712faf057ecd6dd4b
Raidenkyu <joao-carlos.14@hotmail.com>
2020-03-15 16:48:40 +0000

    Added Pause Menu

--- Assets/Scripts/BoltGun.cs
+++ Assets/Scripts/BoltGun.cs
@@ -19,7 +19,7 @@ public class BoltGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && !PauseMenu.GameIsPaused)
         {
             Shoot();
         }
---
+++ Assets/Scripts/PauseOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseOptions : MonoBehaviour
+{
+    public void ResumeGame() {
+
+    }
+
+    public void ReturnMenu() {
+
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
