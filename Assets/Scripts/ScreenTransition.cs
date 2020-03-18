5c9941826b7a556e56641566ed86bee8f3e90295
Raidenkyu <joao-carlos.14@hotmail.com>
2020-03-18 16:16:20 +0000

    Added Backstory, Manual and Enemies list in the Main Menu

---
+++ Assets/Scripts/ScreenTransition.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenTransition : MonoBehaviour
+{
+    public GameObject menuOptions;
+    public GameObject backstory;
+    public GameObject manual;
+    public GameObject enemies;
+
+    public void MenuOptionsNext()
+    {
+        menuOptions.SetActive(false);
+        backstory.SetActive(true);
+    }
+
+    public void BackstoryBack()
+    {
+        backstory.SetActive(false);
+        menuOptions.SetActive(true);
+    }
+
+    public void BackstoryNext()
+    {
+        backstory.SetActive(false);
+        manual.SetActive(true);
+    }
+
+    public void ManualBack()
+    {
+        manual.SetActive(false);
+        backstory.SetActive(true);
+    }
+
+    public void ManualNext()
+    {
+        manual.SetActive(false);
+        enemies.SetActive(true);
+    }
+
+    public void EnemiesBack()
+    {
+        enemies.SetActive(false);
+        backstory.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
